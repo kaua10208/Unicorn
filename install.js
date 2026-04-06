4baa235e17288091fe76a812a15527a7253f0b1d
Kauã <skauako550@icloud.com>
2026-04-06 20:04:35 -0300

    Update download link for Linux release in install.js

--- install.js
+++ install.js
@@ -1,4 +1,4 @@
 document.getElementById('open_source').addEventListener('click', function() {
-    const link = 'https://github.com/kaua10208/Unicorn/releases/download/main-test/ss.zip';
+    const link = 'https://github.com/kaua10208/Unicorn/releases/download/main-test/linux.zip';
     window.location.href = link;
 });
