b5a871ada8ec75fcd237cc49c3a7b989a9fee716
Kauã <skauako550@icloud.com>
2026-04-06 20:11:23 -0300

    Update button ID for Windows package and add alert for future availability

--- install.js
+++ install.js
@@ -2,3 +2,7 @@ document.getElementById('open_source').addEventListener('click', function() {
     const link = 'https://github.com/kaua10208/Unicorn/releases/download/main-test/linux.zip';
     window.location.href = link;
 });
+
+document.getElementById('windows').addEventListener('click', function() {
+    alert('Ainda não temos um pacote para Windows, mas estamos trabalhando nisso! Fique atento para futuras atualizações do projeto!');
+});
